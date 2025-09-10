39667fad99e465ec74db3a8453c7fb88fc99327e
Guadixt <guadalupe.l.tissera@gmail.com>
2025-09-09 23:23:08 -0300

    Version final

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,15 +2,37 @@
 let amigos = []
 
 function agregarAmigo(){
-    let nombre_amigo = document.getElementById("amigo").value
-    if (nombre_amigo == ''){
+    let nombreAmigo = document.getElementById("amigo").value
+    if (nombreAmigo == ''){
         alert("Por favor, inserte un nombre");}
     else{
-            amigos.push(nombre_amigo);}
+            amigos.push(nombreAmigo);}
     console.log(amigos);
     limpiarCaja();
+    listaDeAmigos();
 }
 
 function limpiarCaja() {
     document.querySelector("#amigo").value = '';
+}
+
+function listaDeAmigos(){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0){
+        print("No hay amigos agregados a la lista, por favor ingresa los nombres para continuar");
+        return
+    }
+    let indiceNumerico = Math.floor(Math.random()*amigos.length);
+    let amigoSorteado = amigos[indiceNumerico];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`
 }
