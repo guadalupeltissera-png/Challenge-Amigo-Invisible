5e81cc0bb123468f1c6cb512daa3bcc5144276d8
Guadixt <guadalupe.l.tissera@gmail.com>
2025-09-09 23:02:31 -0300

    Actualizacion del index y app

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,5 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 
-function amigos():
-    
+function agregarAmigo(){
+    let nombre_amigo = document.getElementById("amigo").value
+    if (nombre_amigo == ''){
+        alert("Por favor, inserte un nombre");}
+    else{
+            amigos.push(nombre_amigo);}
+    console.log(amigos);
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+    document.querySelector("#amigo").value = '';
+}
